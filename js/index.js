0da9a4da23ff004f84b59bfd854fe25f87978f6a
EMMANUELLA ETUK <emmanuellaetuk@gmail.com>
2021-03-20 20:59:44 +0100

    animation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+AOS.init();
 window.addEventListener("load", () => {
     document.querySelector("body").classList.add("loaded");
 });
+
+if (screen.width <= 1024) {
+    // $("#our-services-section .row").removeAttr("data-aos");
+    // $("#our-services-section div").removeClass("row");
+    // $("#services").addClass("row");
+}
