61c5d4f732af8c86294e366930a631db74df8fca
EMMANUELLA ETUK <emmanuellaetuk@gmail.com>
2021-03-22 09:45:20 +0100

    responsiveness done

--- js/index.js
+++ js/index.js
@@ -1,12 +1,29 @@
+
+// *************To load animations*****************
 AOS.init();
 window.addEventListener("load", () => {
     document.querySelector("body").classList.add("loaded");
 });
 
-if (screen.width <= 1024) {
-    $("#our-services-section .row").removeAttr("data-aos");
-    $(".d-flex.justify-content-center.col-xl-4").unwrap();
-    $("#services .row").removeClass("row");
-    $("#services").addClass("row");
 
+
+// *************Hamburger icon animation*****************
+document.getElementsByClassName("first-button")[0].onclick = openNavbar;
+
+function openNavbar() {
+    document.getElementsByClassName("navbar-toggler-icon")[0].classList.toggle("open");
+    //document.getElementById("navbarNavDropdown").style.backgroundColor = "rgba(218, 214, 204, 1)";
+    document.getElementsByClassName("navbar")[0].classList.toggle("background");
 }
+
+
+
+
+
+// if (screen.width <= 1030) {
+//     $("#our-services-section .row").removeAttr("data-aos");
+//     $(".d-flex.justify-content-center.col-xl-4").unwrap();
+//     $("#services .row").removeClass("row");
+//     $("#services").addClass("row");
+
+// }
